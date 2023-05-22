47680d5deeb2b0a47601cd70804e8718d5978249
Bin <bin2234@qq.com>
2023-05-22 11:01:48 +0800

    1

---
+++ src/test/java/com/bin/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bin.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
